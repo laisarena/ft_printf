ad7fbd2f1348c5e9dbc959c7169140b9821fa906
Lais Arena <laisarena@gmail.com>
2020-08-19 16:56:21 -0300

    506/514 - Todos os conversores de numero funcionando

--- srcs/ft_integers.c
+++ srcs/ft_integers.c
@@ -6,42 +6,49 @@
 /*   By: laisarena <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 10:31:28 by laisarena         #+#    #+#             */
-/*   Updated: 2020/08/19 13:50:25 by laisarena        ###   ########.fr       */
+/*   Updated: 2020/08/19 16:55:08 by laisarena        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void	ft_printstr(unsigned int pr, unsigned int sign, char *str, char cnv)
+static void	ft_printstr(t_flags flag, unsigned int sign, char *str, char cnv)
 {
+	if (!flag.justify)
+		while (flag.width.val--)
+			if (!flag.zero)
+				ft_putchar_fd(' ', 1);
 	if (sign)
 		ft_putchar_fd('-', 1);
 	if (cnv == 'p')
 		ft_putstr_fd("0x", 1);
-	while (pr--)
+	while (flag.prec.val--)
 		ft_putchar_fd('0', 1);
 	ft_putstr_fd(str, 1);
+	if (flag.justify)
+		while (flag.width.val--)
+			ft_putchar_fd(' ', 1);
 }
 
-static void ft_nbrptrchar(t_flags flag, unsigned int len, unsigned int sign, 
+static void	ft_nbrptrchar(t_flags flag, unsigned int len, unsigned int sign,
 								unsigned int *nbr_pc)
 {
-	if (flag.width.val > flag.prec.val && flag.width.val > len )
+	if (flag.width.val > flag.prec.val && flag.width.val > len)
 		*nbr_pc += flag.width.val;
 	else if (flag.prec.val >= flag.width.val && flag.prec.val >= len)
 		*nbr_pc += flag.prec.val + sign;
-	else 
+	else
 		*nbr_pc += len;
 }
 
 /*
- * PRECISION - the minimum number of digits to be printed for 
+ * PRECISION - the minimum number of digits to be printed for
  *					d, i, o, u, x, and X conversions
  *  - When '0' and precision, '0' is ignored.
  *  - The sign of the number is not include on precision size
- */
+*/
 
-static void	ft_printflag(char *str, t_flags flag, char conversion, 
+static void	ft_printflag(char *str, t_flags flag, char conversion,
 		unsigned int *nbr_pc)
 {
 	unsigned int	len;
@@ -52,27 +59,22 @@ static void	ft_printflag(char *str, t_flags flag, char conversion,
 		len = len + 2;
 	sign = (*str == '-') ? 1 : 0;
 	str = (*str == '-') ? str + 1 : str;
-	ft_nbrptrchar(flag, len, sign , nbr_pc);
+	ft_nbrptrchar(flag, len, sign, nbr_pc);
 	if (flag.prec.val > len - sign)
 		flag.prec.val = flag.prec.val - len + sign;
 	else
 		flag.prec.val = 0;
-	if (flag.justify)
-		ft_printstr(flag.prec.val, sign, str, conversion);
-	while (flag.width.val > len + flag.prec.val
-			&& flag.width.val != 0 && flag.width.val-- != 1)
-	{
-		if (flag.zero && !flag.justify)
-			ft_putchar_fd('0', 1);
-		else
-			ft_putchar_fd(' ', 1);
-	}
-	if (!flag.justify)
-		ft_printstr(flag.prec.val, sign, str, conversion);
+	if (flag.width.val > len + flag.prec.val)
+		flag.width.val = flag.width.val - len - flag.prec.val;
+	else
+		flag.width.val = 0;
+	if (flag.zero && !flag.justify)
+		flag.prec.val = flag.prec.val + flag.width.val;
+	ft_printstr(flag, sign, str, conversion);
 }
 
-void		ft_integers(va_list args, t_flags flag, unsigned int *nbr_pc, 
-									char conversion)
+void		ft_integers(va_list args, t_flags flag, unsigned int *nbr_pc,
+						char conversion)
 {
 	char	*str;
 	int		value;
